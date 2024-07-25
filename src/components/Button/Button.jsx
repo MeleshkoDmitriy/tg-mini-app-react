0e0253ae5b6fce5da682bf6b8ed23030c7c6a312
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-07-25 16:59:05 +0300

    button fixed

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,9 @@
-import styles from './Button.module.css'
+import styles from './Button.module.css';
 
-export const Button = ({children}) => {
+export const Button = ({ children, onClick }) => {
   return (
-    <button className={styles.button}>{children}</button>
-  )
-}
+    <button className={styles.button} onClick={onClick}>
+      {children}
+    </button>
+  );
+};
