e9c3facf8012584b5e99b33f3503064d9e57197b
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-07-25 18:47:17 +0300

    form fix

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,12 +1,41 @@
+import { useState } from 'react';
 import { Button } from '../Button/Button';
 import styles from './Form.module.css';
 
 export const Form = () => {
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const handleChangeName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleChangePhone = (event) => {
+    setPhone(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Name: ${name}, Phone: ${phone}`);
+  };
+
   return (
-    <form className={styles.form}>
-      <input className={styles.input} type="text" placeholder='your name' />
-      <input className={styles.input} type="text" placeholder='phone number' />
-      <Button>Отправить</Button>
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <input
+        className={styles.input}
+        onChange={handleChangeName}
+        type="text"
+        placeholder="your name"
+        value={name}
+      />
+      <input
+        className={styles.input}
+        onChange={handleChangePhone}
+        type="text"
+        placeholder="phone number"
+        value={phone}
+      />
+      <Button type="submit">Отправить</Button>
     </form>
-  )
-}
+  );
+};
