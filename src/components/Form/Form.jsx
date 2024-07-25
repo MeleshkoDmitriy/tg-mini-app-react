498ec861aa3ecaf7fa9761ce30a606e92cac12f6
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-07-25 19:45:37 +0300

    sendData

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,26 +1,41 @@
-import { useEffect, useState } from 'react';
-import { Button } from '../Button/Button';
+import { useCallback, useEffect, useState } from 'react';
 import styles from './Form.module.css';
 import { useTelegram } from '../../hooks/useTelegram';
 
 export const Form = () => {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
-  const {tg} = useTelegram();
+  const { tg } = useTelegram();
+
+  const onSendData = useCallback(() => {
+    const data = {
+      name,
+      phone,
+    };
+
+    tg.sendData(JSON.stringify(data));
+  }, [name, phone]);
 
   useEffect(() => {
-    if(!name || !phone) {
+    tg.onEvent('mainButtonClicked', onSendData);
+    return () => {
+      tg.offEvent('mainButtonClicked', onSendData);
+    };
+  }, [onSendData]);
+
+  useEffect(() => {
+    if (!name || !phone) {
       tg.MainButton.hide();
     } else {
       tg.MainButton.show();
     }
-  },[name, phone]);
+  }, [name, phone]);
 
   useEffect(() => {
     tg.MainButton.setParams({
       text: 'Send data',
-    })
-  }, [])
+    });
+  }, []);
 
   const handleChangeName = (event) => {
     setName(event.target.value);
