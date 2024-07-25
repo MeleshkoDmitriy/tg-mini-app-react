05401319fe395cfce48a11baec15e1e37b353e8b
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-07-25 15:07:52 +0300

    mainButton added

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { HomePage } from './pages/HomePage/HomePage';
 import { FormPage } from './pages/FormPage/FormPage';
+import { SDKProvider, initMainButton } from '@telegram-apps/sdk-react';
 
 const routes = createBrowserRouter([
   { path: '/', element: <HomePage /> },
   { path: '/form', element: <FormPage /> },
 ]);
 
+const [mainButton] = initMainButton();
+
+useEffect(() => {
+  mainButton.show();
+}, [])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
+    <SDKProvider acceptCustomStyles debug>
+      <RouterProvider router={routes} />
+    </SDKProvider>
   </React.StrictMode>,
 );
