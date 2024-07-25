3d04ec2d9e84b90cb746b50ee8afca3bc46604cf
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-07-25 15:38:03 +0300

    canceled sdk

--- src/main.jsx
+++ src/main.jsx
@@ -4,23 +4,22 @@ import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { HomePage } from './pages/HomePage/HomePage';
 import { FormPage } from './pages/FormPage/FormPage';
-import { SDKProvider, initMainButton } from '@telegram-apps/sdk-react';
+// import { SDKProvider, initMainButton } from '@telegram-apps/sdk-react';
 
 const routes = createBrowserRouter([
   { path: '/', element: <HomePage /> },
   { path: '/form', element: <FormPage /> },
 ]);
 
-const [mainButton] = initMainButton();
-
-useEffect(() => {
-  mainButton.show();
-}, [])
+// const [mainButton] = initMainButton();
+// useEffect(() => {
+//   mainButton.show();
+// }, [])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SDKProvider acceptCustomStyles debug>
+    {/* <SDKProvider acceptCustomStyles debug> */}
       <RouterProvider router={routes} />
-    </SDKProvider>
+    {/* </SDKProvider> */}
   </React.StrictMode>,
 );
