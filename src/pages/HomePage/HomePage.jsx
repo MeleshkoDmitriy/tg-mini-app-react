5dfa1fa04790ec83ed61669c4a1563050d77d911
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-07-25 15:44:22 +0300

    added tg

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,24 @@
 import { Link } from 'react-router-dom';
 import styles from './HomePage.module.css';
+import { useEffect } from 'react';
+
+const tg = window.Telegram.WebApp;
 
 export const HomePage = () => {
+  useEffect(() => {
+    tg.ready();
+    tg.expand();
+  }, [])
 
+  const onCloseClick = () => {
+    tg.close();
+  };
 
   return (
     <section>
       <h1>HomePage</h1>
       <Link to={`/form`}><button>Show form page</button></Link>
+      <button onClick={onCloseClick}>Закрыть</button>
     </section>
   );
 };
