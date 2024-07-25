6daa306844d513ff36a5797e4a4716e696a26f0f
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-07-25 14:53:18 +0300

    added link

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,13 @@
-import styles from './HomePage.module.css'
+import { Link } from 'react-router-dom';
+import styles from './HomePage.module.css';
 
 export const HomePage = () => {
+
+
   return (
-    <div>HomePage</div>
-  )
-}
+    <section>
+      <h1>HomePage</h1>
+      <Link to={`/form`}><button>Show form page</button></Link>
+    </section>
+  );
+};
