352ea42e2cd3fb0d693b3712a1136df409a9524e
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-07-26 11:09:07 +0300

    replaced form

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import { Button } from '../../components/Button/Button';
 import { Header } from '../../components/Header/Header';
 import { useTelegram } from '../../hooks/useTelegram';
+import { Form } from '../../components/Form/Form';
 
 export const HomePage = () => {
   const { tg, onToggle } = useTelegram();
@@ -18,6 +19,7 @@ export const HomePage = () => {
       <Header />
       <h1>HomePage</h1>
       <main className={styles.main}>
+        <Form />
         <Link to={`/form`}>
           <Button>Show form page</Button>
         </Link>
