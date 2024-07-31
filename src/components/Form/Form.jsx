86cf59f32bbd59d76b7088e93560c88691ed6787
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-07-31 15:14:16 +0300

    fixed

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -69,9 +69,6 @@ export const Form = () => {
         placeholder="phone number"
         value={phone}
       />
-      <button onClick={onSendData} type="button" className={styles.button}>
-        send data to server
-      </button>
     </form>
   );
 };
