c7293a3083ee91983cf42662bd4e51114693ed18
Meleshko Dmitriy <dimitrusmeleskausas@gmail.com>
2024-07-31 15:55:51 +0300

    fixed

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import styles from './Button.module.css';
 
 export const Button = ({ children, onClick }) => {
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={styles.button} onClick={onClick} type='button'>
       {children}
     </button>
   );
--- src/pages/SickPage/SickPage.jsx
+++ src/pages/SickPage/SickPage.jsx
@@ -1,10 +1,11 @@
+import { SickForm } from '../../components/Forms/SickForm/SickForm'
 import styles from './SickPage.module.css'
 
 export const SickPage = () => {
   return (
     <div className={styles.wrapper}>
       <h1>Заболел</h1>
-      <SickPage />
+      <SickForm />
     </div>
   )
 }
